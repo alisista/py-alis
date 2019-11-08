b0eb8df997cc5bbe34818f63b59c9daa458772c2
hoosan <hoosanalis@gmail.com>
2019-11-08 16:23:09 +0900

    finalize

--- setup.py
+++ setup.py
@@ -11,12 +11,15 @@ with open('README.rst') as f:
 with open('LICENSE') as f:
     license = f.read()
 
+
 setup(
     name='alis',
-    version='0.0.3',
+    version='0.0.4',
     description='ALIS API Python Client',
+    long_description_content_type="text/x-rst",
     long_description=readme,
     author='hoosan',
+    author_email='hoosanalis@gmail.com',
     url='https://github.com/alisista/py-alis',
     license=license,
     packages=find_packages(),
@@ -24,6 +27,8 @@ setup(
         "promise",
         "requests",
         "warrant"
-    ]
+    ],
+    setup_requires=["pytest-runner"],
+    tests_require=["pytest", "pytest-cov"]
 )
 
